16be9c77ebed62c9db345eba42608f4db77fa5ba
Abdul Moezm <abdulmoez404@gmail.com>
2024-06-12 22:09:22 +0500

    Using Old laravel routing style for controllers

--- expt_lvl/routes/web.php
+++ expt_lvl/routes/web.php
@@ -1,12 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\BankAccountsController;
-use App\Http\Controllers\IncomeController;
-use App\Http\Controllers\ExpenseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,25 +17,25 @@ use App\Http\Controllers\ExpenseController;
 //     return view('welcome');
 // });
 
-Route::get('/', [LoginController::class, 'login'])->name('login');
-Route::get('/register', [LoginController::class, 'register'])->name('register');
-Route::post('/login_process', [LoginController::class, 'loginProcess'])->name('login_process');
+Route::get('/', 'LoginController@login')->name('login');
+Route::get('/register', 'LoginController@register')->name('register');
+Route::post('/login_process', 'LoginController@loginProcess')->name('login_process');
 
 Route::group(['middleware' => ['normaluserlogin']], function () {
 
-    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+    Route::get('/dashboard', 'DashboardController@dashboard')->name('dashboard');
 
-    Route::get('/dashboard/income', [IncomeController::class, 'income'])->name('income');
-    Route::post('/income_process', [IncomeController::class, 'incomeProcess'])->name('income_process');
+    Route::get('/dashboard/income', 'IncomeController@income')->name('income');
+    Route::post('/income_process', 'IncomeController@incomeProcess')->name('income_process');
 
-    Route::get('/dashboard/expense', [ExpenseController::class, 'expense'])->name('expense');
-    Route::post('/expense_process', [ExpenseController::class, 'expenseProcess'])->name('expense_process');
+    Route::get('/dashboard/expense', 'ExpenseController@expense')->name('expense');
+    Route::post('/expense_process', 'ExpenseController@expenseProcess')->name('expense_process');
 
-    Route::get('/dashboard/bank_accounts', [BankAccountsController::class, 'bankAccounts'])->name('bank_accounts');
-    Route::post('/bank_accounts_process', [BankAccountsController::class, 'bankAccountsProcess'])->name('bank_accounts_process');
+    Route::get('/dashboard/bank_accounts', 'BankAccountsController@bankAccounts')->name('bank_accounts');
+    Route::post('/bank_accounts_process', 'BankAccountsController@bankAccountsProcess')->name('bank_accounts_process');
 
-    Route::get('/dashboard/category', [CategoryController::class, 'category'])->name('category');
-    Route::post('/category_process', [CategoryController::class, 'categoryProcess'])->name('category_process');
+    Route::get('/dashboard/category', 'CategoryController@category')->name('category');
+    Route::post('/category_process', 'CategoryController@categoryProcess')->name('category_process');
 
 });
 
